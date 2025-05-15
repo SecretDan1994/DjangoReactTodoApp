f1acadcd808a680bb4983b6a32f07a039e95f08d
SecretDan1994 <DanielLlerena1994@gmail.com>
2025-05-15 15:10:44 -0400

    Allow CORS for HTTP Requests from React.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -43,9 +43,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'todo',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -72,6 +74,14 @@ TEMPLATES = [
         },
     },
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+]
+
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_METHODS = True
+CORS_ALLOW_HEADERS = True
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
